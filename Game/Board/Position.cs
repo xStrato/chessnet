d7553064c89c64fb1887e6c129e17f99a3ea948f
xStrato <metalofgear@hotmail.com>
2020-07-19 23:08:31 -0300

    feat: added Position class for the game board

---
+++ Game/Board/Position.cs
@@ -0,0 +1,16 @@
+namespace chessnet.Game.Board
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString() => $"Settled: row {Row}, cloumns {Column}.";
+    }
+}
